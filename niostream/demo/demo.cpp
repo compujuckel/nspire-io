ebc1843537b0b934f6eb8c81b1507d34fa22eb3d
compujuckel <compujuckel@gmail.com>
2013-05-30 21:30:59 +0200

    switching to niostream instead of console_base

--- niostream/demo/demo.cpp
+++ niostream/demo/demo.cpp
@@ -1,10 +1,10 @@
-#include <niostream/console_base.hpp>
+#include <niostream/niostream.hpp>
 
 int main(void)
 {
-	nio::console_base csl(nio::MAX_COLS,nio::MAX_ROWS,0,0,nio::COLOR_WHITE,nio::COLOR_BLACK,true);
+	nio::niostream csl(nio::MAX_COLS,nio::MAX_ROWS,0,0,nio::COLOR_WHITE,nio::COLOR_BLACK,true);
 	
-	csl.puts("LOL");
+	csl << "LOL";
 	
 	wait_key_pressed();
 	return 0;
