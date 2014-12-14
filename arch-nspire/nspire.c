21f5926200a8964ec51d047b9bb3834b6bf4d48a
Fabian Vogt <fabian@ritter-vogt.de>
2014-12-14 21:15:57 +0100

    Tidy up a bit and return 0 if invalid key combo pressed in nio_ascii_get
    -Also improved speed slightly

--- arch-nspire/nspire.c
+++ arch-nspire/nspire.c
@@ -64,92 +64,47 @@ unsigned short getPaletteColor(unsigned int color)
 					0x5aab, 0xfaab, 0x5feb, 0xffeb, 0x5abf, 0xfabf, 0x5fff, 0xffff};
 	int rbtable[6] = {0,6,12,18,24,31};
 	int gtable[6] = {0,12,25,37,50,63};
-        unsigned int c = color;
-        unsigned int d;
-        if(c < 16)
-        {
-                return palette[c];
-        }
-        else if(c < 232)
-        {
-                d = c-16;
-                return (rbtable[d/36]<<11)+(gtable[(d/6)%6]<<5)+rbtable[d%6];
-        }
-        else if(c < 256)
-        {
-                d = c-232;
-                return ((d+1)<<11)+((d*2+2)<<5)+(d+1);
-        }
+	unsigned int c = color;
+	unsigned int d;
+	if(c < 16)
+	{
+			return palette[c];
+	}
+	else if(c < 232)
+	{
+			d = c-16;
+			return (rbtable[d/36]<<11)+(gtable[(d/6)%6]<<5)+rbtable[d%6];
+	}
+	else if(c < 256)
+	{
+			d = c-232;
+			return ((d+1)<<11)+((d*2+2)<<5)+(d+1);
+	}
 	return 0;
 }
 
-static void nio_pixel_set_4bpp(void* screen, int x, int y, unsigned short c)
-{
-	unsigned char* p = (unsigned char*)(screen + ((x >> 1) + (y << 7) + (y << 5)));
-	*p = (x & 1) ? ((*p & 0xF0) | getBW(c)) : ((*p & 0x0F) | (getBW(c) << 4));
-}
-
-static void nio_pixel_set_16bpp(void* screen, int x, int y, unsigned short c)
-{
-	unsigned char* ptr = screen + sizeof(uint16_t) * (x + SCREEN_WIDTH * y);
-	*(unsigned short*)ptr = c;
-}
-
 // by totorigolo
-void nio_pixel_set(int x, int y, unsigned int color)
+void nio_pixel_set(unsigned int x, unsigned int y, unsigned int c)
 {
-	if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT)
+	if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
 		return;
+	
+	c = getPaletteColor(c);
 
 	if(lcd_isincolor())
-		nio_pixel_set_16bpp(SCREEN_BASE_ADDRESS, x, y, getPaletteColor(color));
-	else
-		nio_pixel_set_4bpp(SCREEN_BASE_ADDRESS, x, y, getPaletteColor(color));
-}
-
-// Disable VRAM support at the moment. There are issues with consoles that are not fullscreen and VRAM has not really any pros.
-#ifdef NIO_ENABLE_VRAM
-void* VRAM = NULL;
-
-void nio_vram_pixel_set(const int x, const int y, const unsigned int color)
-{
-	if(VRAM == NULL)
 	{
-		VRAM = malloc(SCREEN_BYTES_SIZE);
-		if(VRAM == NULL)
-			exit_with_error(__FUNCTION__,"malloc failed");
-		memcpy(VRAM,SCREEN_BASE_ADDRESS,SCREEN_BYTES_SIZE);
+		unsigned short* ptr = SCREEN_BASE_ADDRESS + sizeof(uint16_t) * (x + SCREEN_WIDTH * y);
+		*ptr = c;
 	}
-	
-	unsigned short c = getPaletteColor(color);
-	
-	if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT)
-        return;
-
-    // 4 bpp
-    if(!lcd_isincolor())
-    {
-        unsigned char* p = (unsigned char*)(VRAM + ((x >> 1) + (y << 7) + (y << 5)));
-        *p = (x & 1) ? ((*p & 0xF0) | getBW(c)) : ((*p & 0x0F) | (getBW(c) << 4));
-    }
-    // 16 bpp
-    else
-    {
-        unsigned char *scr_base = VRAM;
-        unsigned char *ptr;
-        ptr = scr_base + sizeof(uint16_t) * (x + SCREEN_WIDTH * y);
-        *(unsigned short*)ptr = c;
-    }
+	else
+	{
+		c = getBW(c);
+		unsigned char* p = (unsigned char*)(SCREEN_BASE_ADDRESS + ((x >> 1) + (y << 7) + (y << 5)));
+		*p = (x & 1) ? ((*p & 0xF0) | c) : ((*p & 0x0F) | (c << 4));
+	}
 }
 
-void nio_vram_draw(void)
-{
-	memcpy(SCREEN_BASE_ADDRESS,VRAM,SCREEN_BYTES_SIZE);
-}
-
-#else
-
-void nio_vram_pixel_set(const int x, const int y, const unsigned int color)
+void nio_vram_pixel_set(const unsigned int x, const unsigned int y, const unsigned int color)
 {
 	nio_pixel_set(x,y,color);
 }
@@ -172,105 +127,88 @@ void nio_vram_fill(unsigned int color)
 }
 
 void nio_vram_draw(void)
-{
-
-}
-
-#endif // NIO_ENABLE_VRAM
+{}
 
 unsigned int nio_cursor_clock(void) {
     return *(volatile unsigned*)0x90090000;
 }
 
-static BOOL shift = FALSE;
-static BOOL ctrl = FALSE;
-static char shiftKey(const char normalc, const char shiftc)
-{
-	if(isKeyPressed(KEY_NSPIRE_SHIFT)) 
-		return shiftc;
-	else return normalc;
-}
-static char shiftOrCtrlKey(const char normalc, const char shiftc, const char ctrlc)
-{
-	if(isKeyPressed(KEY_NSPIRE_CTRL))
-		return ctrlc;
-	else if(isKeyPressed(KEY_NSPIRE_SHIFT))
-		return shiftc;
-	else return normalc;
-}
+#define SHIFTCTRL(x, y, z) (ctrl ? (z) : shift ? (y) : (x))
+#define SHIFT(x, y) SHIFTCTRL(x, y, 0)
+#define NORMAL(x) SHIFT(x, 0)
 
 char nio_ascii_get(int* adaptive_cursor_state)
 {
-	ctrl = isKeyPressed(KEY_NSPIRE_CTRL);
-	shift = isKeyPressed(KEY_NSPIRE_SHIFT);
+	BOOL ctrl = isKeyPressed(KEY_NSPIRE_CTRL);
+	BOOL shift = isKeyPressed(KEY_NSPIRE_SHIFT);
 
-	*adaptive_cursor_state = ctrl ? 4 : shift ? 1 : 0;
+	*adaptive_cursor_state = SHIFTCTRL(0, 1, 4);
 	
 	if(isKeyPressed(KEY_NSPIRE_ESC)) return NIO_KEY_ESC;
 	
 	// Characters
-	if(isKeyPressed(KEY_NSPIRE_A)) return shiftKey('a','A');
-	if(isKeyPressed(KEY_NSPIRE_B)) return shiftKey('b','B');
-	if(isKeyPressed(KEY_NSPIRE_C)) return shiftKey('c','C');
-	if(isKeyPressed(KEY_NSPIRE_D)) return shiftKey('d','D');
-	if(isKeyPressed(KEY_NSPIRE_E)) return shiftKey('e','E');
-	if(isKeyPressed(KEY_NSPIRE_F)) return shiftKey('f','F');
-	if(isKeyPressed(KEY_NSPIRE_G)) return shiftKey('g','G');
-	if(isKeyPressed(KEY_NSPIRE_H)) return shiftKey('h','H');
-	if(isKeyPressed(KEY_NSPIRE_I)) return shiftKey('i','I');
-	if(isKeyPressed(KEY_NSPIRE_J)) return shiftKey('j','J');
-	if(isKeyPressed(KEY_NSPIRE_K)) return shiftKey('k','K');
-	if(isKeyPressed(KEY_NSPIRE_L)) return shiftKey('l','L');
-	if(isKeyPressed(KEY_NSPIRE_M)) return shiftKey('m','M');
-	if(isKeyPressed(KEY_NSPIRE_N)) return shiftKey('n','N');
-	if(isKeyPressed(KEY_NSPIRE_O)) return shiftKey('o','O');
-	if(isKeyPressed(KEY_NSPIRE_P)) return shiftKey('p','P');
-	if(isKeyPressed(KEY_NSPIRE_Q)) return shiftKey('q','Q');
-	if(isKeyPressed(KEY_NSPIRE_R)) return shiftKey('r','R');
-	if(isKeyPressed(KEY_NSPIRE_S)) return shiftKey('s','S');
-	if(isKeyPressed(KEY_NSPIRE_T)) return shiftKey('t','T');
-	if(isKeyPressed(KEY_NSPIRE_U)) return shiftKey('u','U');
-	if(isKeyPressed(KEY_NSPIRE_V)) return shiftKey('v','V');
-	if(isKeyPressed(KEY_NSPIRE_W)) return shiftKey('w','W');
-	if(isKeyPressed(KEY_NSPIRE_X)) return shiftKey('x','X');
-	if(isKeyPressed(KEY_NSPIRE_Y)) return shiftKey('y','Y');
-	if(isKeyPressed(KEY_NSPIRE_Z)) return shiftKey('z','Z');
+	if(isKeyPressed(KEY_NSPIRE_A)) return SHIFT('a','A');
+	if(isKeyPressed(KEY_NSPIRE_B)) return SHIFT('b','B');
+	if(isKeyPressed(KEY_NSPIRE_C)) return SHIFT('c','C');
+	if(isKeyPressed(KEY_NSPIRE_D)) return SHIFT('d','D');
+	if(isKeyPressed(KEY_NSPIRE_E)) return SHIFT('e','E');
+	if(isKeyPressed(KEY_NSPIRE_F)) return SHIFT('f','F');
+	if(isKeyPressed(KEY_NSPIRE_G)) return SHIFT('g','G');
+	if(isKeyPressed(KEY_NSPIRE_H)) return SHIFT('h','H');
+	if(isKeyPressed(KEY_NSPIRE_I)) return SHIFT('i','I');
+	if(isKeyPressed(KEY_NSPIRE_J)) return SHIFT('j','J');
+	if(isKeyPressed(KEY_NSPIRE_K)) return SHIFT('k','K');
+	if(isKeyPressed(KEY_NSPIRE_L)) return SHIFT('l','L');
+	if(isKeyPressed(KEY_NSPIRE_M)) return SHIFT('m','M');
+	if(isKeyPressed(KEY_NSPIRE_N)) return SHIFT('n','N');
+	if(isKeyPressed(KEY_NSPIRE_O)) return SHIFT('o','O');
+	if(isKeyPressed(KEY_NSPIRE_P)) return SHIFT('p','P');
+	if(isKeyPressed(KEY_NSPIRE_Q)) return SHIFT('q','Q');
+	if(isKeyPressed(KEY_NSPIRE_R)) return SHIFT('r','R');
+	if(isKeyPressed(KEY_NSPIRE_S)) return SHIFT('s','S');
+	if(isKeyPressed(KEY_NSPIRE_T)) return SHIFT('t','T');
+	if(isKeyPressed(KEY_NSPIRE_U)) return SHIFT('u','U');
+	if(isKeyPressed(KEY_NSPIRE_V)) return SHIFT('v','V');
+	if(isKeyPressed(KEY_NSPIRE_W)) return SHIFT('w','W');
+	if(isKeyPressed(KEY_NSPIRE_X)) return SHIFT('x','X');
+	if(isKeyPressed(KEY_NSPIRE_Y)) return SHIFT('y','Y');
+	if(isKeyPressed(KEY_NSPIRE_Z)) return SHIFT('z','Z');
 	
 	// Numbers
-	if(isKeyPressed(KEY_NSPIRE_0)) return '0';
-	if(isKeyPressed(KEY_NSPIRE_1)) return '1';
-	if(isKeyPressed(KEY_NSPIRE_2)) return '2';
-	if(isKeyPressed(KEY_NSPIRE_3)) return '3';
-	if(isKeyPressed(KEY_NSPIRE_4)) return '4';
-	if(isKeyPressed(KEY_NSPIRE_5)) return '5';
-	if(isKeyPressed(KEY_NSPIRE_6)) return '6';
-	if(isKeyPressed(KEY_NSPIRE_7)) return '7';
-	if(isKeyPressed(KEY_NSPIRE_8)) return '8';
-	if(isKeyPressed(KEY_NSPIRE_9)) return '9';
+	if(isKeyPressed(KEY_NSPIRE_0)) return NORMAL('0');
+	if(isKeyPressed(KEY_NSPIRE_1)) return NORMAL('1');
+	if(isKeyPressed(KEY_NSPIRE_2)) return NORMAL('2');
+	if(isKeyPressed(KEY_NSPIRE_3)) return NORMAL('3');
+	if(isKeyPressed(KEY_NSPIRE_4)) return NORMAL('4');
+	if(isKeyPressed(KEY_NSPIRE_5)) return NORMAL('5');
+	if(isKeyPressed(KEY_NSPIRE_6)) return NORMAL('6');
+	if(isKeyPressed(KEY_NSPIRE_7)) return NORMAL('7');
+	if(isKeyPressed(KEY_NSPIRE_8)) return NORMAL('8');
+	if(isKeyPressed(KEY_NSPIRE_9)) return NORMAL('9');
 	
 	// Symbols
-	if(isKeyPressed(KEY_NSPIRE_COMMA))		return shiftKey(',',';');
-	if(isKeyPressed(KEY_NSPIRE_PERIOD)) 	return shiftKey('.',':');
-	if(isKeyPressed(KEY_NSPIRE_COLON))		return ':';
-	if(isKeyPressed(KEY_NSPIRE_LP))			return shiftOrCtrlKey('(','[',']');
-	if(isKeyPressed(KEY_NSPIRE_RP))			return +shiftOrCtrlKey(')','{','}');
-	if(isKeyPressed(KEY_NSPIRE_SPACE))		return shiftKey(' ','_');
-	if(isKeyPressed(KEY_NSPIRE_DIVIDE))		return shiftKey('/','\\');
-	if(isKeyPressed(KEY_NSPIRE_MULTIPLY))	return shiftKey('*','\"');
-	if(isKeyPressed(KEY_NSPIRE_MINUS))		return shiftOrCtrlKey('-','<', '_');
-	if(isKeyPressed(KEY_NSPIRE_NEGATIVE))	return shiftKey('-','_');
-	if(isKeyPressed(KEY_NSPIRE_PLUS))		return shiftKey('+', '>');
-	if(isKeyPressed(KEY_NSPIRE_EQU))		return shiftKey('=', '|');
-	if(isKeyPressed(KEY_NSPIRE_LTHAN))		return '<';
-	if(isKeyPressed(KEY_NSPIRE_GTHAN))		return '>';
-	if(isKeyPressed(KEY_NSPIRE_QUOTE))		return '\"';
-	if(isKeyPressed(KEY_NSPIRE_APOSTROPHE))	return '\'';
-	if(isKeyPressed(KEY_NSPIRE_QUES))		return shiftKey('?','!');
-	if(isKeyPressed(KEY_NSPIRE_QUESEXCL))	return shiftKey('?','!');
-	if(isKeyPressed(KEY_NSPIRE_BAR))		return '|';
-	if(isKeyPressed(KEY_NSPIRE_EXP))		return '^';
-	if(isKeyPressed(KEY_NSPIRE_EE))		return shiftOrCtrlKey('&','%', '@');
-	if(isKeyPressed(KEY_NSPIRE_ENTER))		return shiftKey('\n','~');
+	if(isKeyPressed(KEY_NSPIRE_COMMA))		return SHIFT(',',';');
+	if(isKeyPressed(KEY_NSPIRE_PERIOD)) 	return SHIFT('.',':');
+	if(isKeyPressed(KEY_NSPIRE_COLON))		return NORMAL(':');
+	if(isKeyPressed(KEY_NSPIRE_LP))			return SHIFTCTRL('(','[',']');
+	if(isKeyPressed(KEY_NSPIRE_RP))			return SHIFTCTRL(')','{','}');
+	if(isKeyPressed(KEY_NSPIRE_SPACE))		return SHIFT(' ','_');
+	if(isKeyPressed(KEY_NSPIRE_DIVIDE))		return SHIFT('/','\\');
+	if(isKeyPressed(KEY_NSPIRE_MULTIPLY))	return SHIFT('*','\"');
+	if(isKeyPressed(KEY_NSPIRE_MINUS))		return SHIFTCTRL('-','<', '_');
+	if(isKeyPressed(KEY_NSPIRE_NEGATIVE))	return SHIFT('-','_');
+	if(isKeyPressed(KEY_NSPIRE_PLUS))		return SHIFT('+', '>');
+	if(isKeyPressed(KEY_NSPIRE_EQU))		return SHIFT('=', '|');
+	if(isKeyPressed(KEY_NSPIRE_LTHAN))		return NORMAL('<');
+	if(isKeyPressed(KEY_NSPIRE_GTHAN))		return NORMAL('>');
+	if(isKeyPressed(KEY_NSPIRE_QUOTE))		return NORMAL('\"');
+	if(isKeyPressed(KEY_NSPIRE_APOSTROPHE))	return NORMAL('\'');
+	if(isKeyPressed(KEY_NSPIRE_QUES))		return SHIFT('?','!');
+	if(isKeyPressed(KEY_NSPIRE_QUESEXCL))	return SHIFT('?','!');
+	if(isKeyPressed(KEY_NSPIRE_BAR))		return NORMAL('|');
+	if(isKeyPressed(KEY_NSPIRE_EXP))		return NORMAL('^');
+	if(isKeyPressed(KEY_NSPIRE_EE))		return SHIFTCTRL('&','%', '@');
+	if(isKeyPressed(KEY_NSPIRE_ENTER))		return SHIFT('\n','~');
 	
 	// Special chars
 	if(isKeyPressed(KEY_NSPIRE_DEL))		return '\b';
